457b28cc68e39e45a1c4caa9674348bf3c75cb42
tomlion <qycpublic@gmail.com>
2013-12-31 16:40:53 +0800

    Trim API response and shape it as an object

--- bitbot-bitfinex.gemspec
+++ bitbot-bitfinex.gemspec
@@ -4,9 +4,11 @@ Gem::Specification.new do |s|
   s.date        = '2013-12-30'
   s.summary     = "A bitbot adapter for bitfinex"
   s.description = "A bitbot adapter for bitfinex"
-  s.authors     = ["dasixi"]
+  s.authors     = ["dasixi", "tomlion"]
   s.email       = 'i@tomlion.com'
-  s.files       = ["lib/bitbot-bitfinex.rb"]
   s.license       = 'GPL V2'
+  s.files       = `git ls-files`.split("\n")
+  s.homepage    = 'https://github.com/dasixi/bitbot-bitfinex'
   s.add_dependency 'bitfinex'
+  s.add_dependency 'bitbot'
 end
--- lib/bitbot-bitfinex.rb
+++ lib/bitbot-bitfinex.rb
@@ -4,29 +4,49 @@ module BitBot
   module Bitfinex
     ### INFO ###
     def ticker
-      client.ticker
+      map = {last_price: :last, mid: nil}
+      Ticker.new rekey(client.ticker, map)
+    end
+
+    #price, amount, timestamp
+    def offers
+      resp = client.orderbook
+      asks = resp['asks'].collect do |offer|
+        Offer.new offer
+      end
+      bids = resp['bids'].collect do |offer|
+        Offer.new offer
+      end
+      {asks: asks, bids: bids}
     end
 
     def asks
-      client.orderbook['asks']
+      offers[:asks]
     end
 
     def bids
-      client.orderbook['bids']
+      offers[:bids]
     end
 
     ### TRADES ###
-
+    #{"id"=>4880202, "symbol"=>"btcusd", "exchange"=>nil, "price"=>"1.0", "avg_execution_price"=>"0.0", "side"=>"buy",
+    #"type"=>"exchange limit", "timestamp"=>"1388473334.231488559", "is_live"=>true, "is_cancelled"=>false, "was_forced"=>false,
+    #"original_amount"=>"0.1", "remaining_amount"=>"0.1", "executed_amount"=>"0.0", "order_id"=>4880202}
     def buy(options)
       amount = options[:amount]
       price = options[:price]
-      client.order amount, price, 'exchange limit'
+      resp = client.order amount, price, 'exchange limit'
+      build_order(resp)
     end
 
+    #{"id"=>4880203, "symbol"=>"btcusd", "exchange"=>nil, "price"=>"10000.0", "avg_execution_price"=>"0.0", "side"=>"sell",
+    #"type"=>"exchange limit", "timestamp"=>"1388473337.204447101", "is_live"=>true, "is_cancelled"=>false, "was_forced"=>false,
+    #"original_amount"=>"0.1", "remaining_amount"=>"0.1", "executed_amount"=>"0.0", "order_id"=>4880203}
     def sell(options)
       amount = options[:amount]
       price = options[:price]
-      client.order (-amount), price, 'exchange limit'
+      resp = client.order (-amount), price, 'exchange limit'
+      build_order(resp)
     end
 
     def cancel(order_id)
@@ -35,10 +55,42 @@ module BitBot
 
     ### ACCOUNT ###
     def orders
-      client.orders
+      client.orders.collect do |hash|
+        build_order(hash)
+      end
     end
 
     private
+    def build_order(hash)
+      map = { symbol: nil,
+              exchange: nil,
+              avg_execution_price: :avg_price,
+              type: nil,
+              side: :type,
+              is_live: nil,
+              is_cancelled: nil,
+              was_forced: nil,
+              original_amount: :amount,
+              remaining_amount: :remaining,
+              executed_amount: nil,
+              order_id: nil
+      }
+      order = Order.new rekey(hash, map)
+      order.type = case order.type
+                   when 'buy' then 'bid'
+                   when 'sell' then 'ask'
+                   else raise "Unexcepted order type: #{order.type}"
+                   end
+      order.status = if hash['is_live']
+                       'open'
+                     elsif hash['is_cancelled']
+                       'cancelled'
+                     else
+                       'closed'
+                     end
+      order
+    end
+
     def client
       @client ||= BitFinex.new @key, @secret
     end
